dbbaa4edb1d0aae111c130a8be7fd94f04f30831
kailashTuta <kailashTuta@users.noreply.github.com>
2020-09-06 09:23:38 +0530

    Added Database

--- venv/part_manager.py
+++ venv/part_manager.py
@@ -1,5 +1,6 @@
 from tkinter import *
 
+
 def populate_list():
     print('Populate')
 
@@ -20,35 +21,35 @@ app = Tk()
 
 # Part
 part_text = StringVar()
-part_label = Label(app, text='Part Name', font=('bold',14),pady=20)
+part_label = Label(app, text='Part Name', font=('bold', 14), pady=20)
 part_label.grid(row=0, column=0, sticky=W)
-part_entry = Entry(app,textvariable=part_text)
-part_entry.grid(row = 0, column=1)
+part_entry = Entry(app, textvariable=part_text)
+part_entry.grid(row=0, column=1)
 # Customer
 customer_text = StringVar()
-customer_label = Label(app, text='Customer Name', font=('bold',14))
+customer_label = Label(app, text='Customer', font=('bold', 14))
 customer_label.grid(row=0, column=2, sticky=W)
-customer_entry = Entry(app,textvariable=customer_text)
-customer_entry.grid(row = 0, column=3)
+customer_entry = Entry(app, textvariable=customer_text)
+customer_entry.grid(row=0, column=3)
 # Retailer
 retailer_text = StringVar()
-retailer_label = Label(app, text='Retailer Name', font=('bold',14))
+retailer_label = Label(app, text='Retailer', font=('bold', 14))
 retailer_label.grid(row=1, column=0, sticky=W)
-retailer_entry = Entry(app,textvariable=retailer_text)
-retailer_entry.grid(row = 1, column=1)
+retailer_entry = Entry(app, textvariable=retailer_text)
+retailer_entry.grid(row=1, column=1)
 # Price
 price_text = StringVar()
-price_label = Label(app, text='Price', font=('bold',14))
+price_label = Label(app, text='Price', font=('bold', 14))
 price_label.grid(row=1, column=2, sticky=W)
-price_entry = Entry(app,textvariable=price_text)
-price_entry.grid(row = 1, column=3)
+price_entry = Entry(app, textvariable=price_text)
+price_entry.grid(row=1, column=3)
 # Parts List (Listbox)
 parts_list = Listbox(app, height=8, width=50, border=0)
 parts_list.grid(row=3, column=0, columnspan=3, rowspan=6, pady=20, padx=20)
 # Create scrollbar
 scrollbar = Scrollbar(app)
 scrollbar.grid(row=3, column=3)
-#set scoll to listbox
+# Set scroll to listbox
 parts_list.configure(yscrollcommand=scrollbar.set)
 scrollbar.configure(command=parts_list.yview)
 
@@ -68,5 +69,7 @@ clear_btn.grid(row=2, column=3)
 app.title('Part Manager')
 app.geometry('700x350')
 
+# Populate data
+populate_list()
 # Start Program
 app.mainloop()
